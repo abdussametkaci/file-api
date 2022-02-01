2ca9f37e3b1365704a37eac3ad38746a1121fef7
Abdussamet KACI <abdussamet.kaci@stu.fsm.edu.tr>
2022-02-01 21:20:37 +0300

    add delete file and format

--- src/main/kotlin/com/file/fileapi/service/FileService.kt
+++ src/main/kotlin/com/file/fileapi/service/FileService.kt
@@ -1,51 +1,62 @@
 package com.file.fileapi.service
 
+import com.file.fileapi.configuration.properties.FileProperties
 import com.file.fileapi.domain.model.File
 import com.file.fileapi.exception.NotFoundException
 import com.file.fileapi.repository.FileRepository
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.reactor.mono
 import kotlinx.coroutines.withContext
 import org.springframework.core.io.FileSystemResource
 import org.springframework.core.io.Resource
-import org.springframework.core.io.buffer.DataBufferUtils
 import org.springframework.http.codec.multipart.FilePart
 import org.springframework.stereotype.Service
-import reactor.core.scheduler.Schedulers
-import java.nio.channels.AsynchronousFileChannel
 import java.nio.file.Files
+import java.nio.file.Path
 import java.nio.file.Paths
-import java.nio.file.StandardOpenOption
 import java.util.UUID
 
 @Service
-class FileService(private val fileRepository: FileRepository) {
+class FileService(
+    private val fileRepository: FileRepository,
+    private val fileProperties: FileProperties
+) {
 
     suspend fun upload(filePart: FilePart): File {
+        val basePath = Paths.get(fileProperties.upload)
         val extension = filePart.extension
         val file = fileRepository.save(File(extension = extension))
-        val filename = file.id.toString() + "." + file.extension
 
         withContext(Dispatchers.IO) {
-            Files.createDirectories(Paths.get("./files"))
-            val filePath = Paths.get("./files/$filename").toAbsolutePath().normalize()
-            val path = Files.createFile(filePath)
-            val channel = AsynchronousFileChannel.open(path, StandardOpenOption.WRITE)
-            DataBufferUtils.write(filePart.content(), channel, 0)
-                .publishOn(Schedulers.boundedElastic())
-                .doOnComplete {
-                    channel.close()
-                }
-                .subscribe()
+            Files.createDirectories(basePath)
         }
+
+        mono {
+            filePart
+        }
+            .flatMap { fp ->
+                fp.transferTo(basePath.resolve(file.getName()))
+            }
+            .subscribe()
 
         return file
     }
 
     suspend fun download(id: UUID): Resource {
         val file = fileRepository.findById(id) ?: throw NotFoundException("error.file.not-found", args = arrayOf(id))
-        val filename = file.id.toString() + "." + file.extension
-        val path = Paths.get("./files/$filename").toAbsolutePath().normalize()
-        return FileSystemResource(path.toString())
+        return FileSystemResource(getPath(file.getName()).toString())
+    }
+
+    suspend fun delete(id: UUID) {
+        fileRepository.findById(id)
+            ?.let {
+                fileRepository.deleteById(id)
+                Files.deleteIfExists(getPath(it.getName()))
+            } ?: throw NotFoundException("error.file.not-found", args = arrayOf(id))
+    }
+
+    private suspend fun getPath(filename: String): Path {
+        return Paths.get("${fileProperties.upload}/$filename").toAbsolutePath().normalize()
     }
 
     private val FilePart.extension: String
