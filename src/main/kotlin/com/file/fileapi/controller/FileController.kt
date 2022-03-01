e8ac06cdd65dcbf9822d7866e7571006ca4c206a
Abdussamet KACI <abdussamet.kaci@stu.fsm.edu.tr>
2022-03-01 17:51:42 +0300

    remove unnecessary produces in get endpoint

--- src/main/kotlin/com/file/fileapi/controller/FileController.kt
+++ src/main/kotlin/com/file/fileapi/controller/FileController.kt
@@ -3,7 +3,6 @@ package com.file.fileapi.controller
 import com.file.fileapi.domain.model.File
 import com.file.fileapi.service.FileService
 import org.springframework.core.io.Resource
-import org.springframework.http.MediaType.APPLICATION_OCTET_STREAM_VALUE
 import org.springframework.http.codec.multipart.FilePart
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
@@ -23,7 +22,7 @@ class FileController(private val fileService: FileService) {
         return fileService.upload(filePart)
     }
 
-    @GetMapping("/{id}", produces = [APPLICATION_OCTET_STREAM_VALUE])
+    @GetMapping("/{id}")
     suspend fun download(@PathVariable id: UUID): Resource {
         return fileService.download(id)
     }
